939521ef992d70a381cfcfef6806cd2ed3e3eb80
nahara7 <mayanahara@gmail.com>
2021-02-06 17:01:21 -0500

    heroku deploy 1

--- src/main/java/com/nahara/toka/repository/WalletRepository.java
+++ src/main/java/com/nahara/toka/repository/WalletRepository.java
@@ -1,6 +1,4 @@
-/*package com.newjerseysoftware.hederaDemo.repository;
-
-import com.newjerseysoftware.hederaDemo.model.Wallet;
+/*
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
